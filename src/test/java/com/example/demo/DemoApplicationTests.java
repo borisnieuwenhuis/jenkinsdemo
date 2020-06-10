aaf2a4142dcf6a872acbf2ca17c83a8ab0515377
Boris Nieuwenhuis <bnieuwenhuis@tripactions.com>
2020-06-10 22:17:01 +0200

    integration tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testAsserts() {
+		assertEquals(1, 1);
 	}
 
 }
